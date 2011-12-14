bdbfc66741a7fd356dde0e80a5bae71225205c22
Thiago de Mello Bueno <thiago@madeofpeople.org>
2011-12-14 14:52:56 -0500

    Added resources

---
+++ js/site.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	// add drop shadow to any element with the '.shadow' class
+	var shadows = $(document).find('.shadow');
+	shadows.each( function( i, element ){
+		nycga.ui.addDropShadow( element );
+	});
+	// show a tooltip for each element with the '.hoverMenu' class
+	// code.drewwilson.com/entry/tiptip-jquery-plugin
+	nycga.ui.addToolTips();
+});
